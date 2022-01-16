1bdea3b85466d2d2e09da8c2d0f6472fbe8b1fdb
jbrems <jonasbrems@gmail.com>
2022-01-16 16:05:26 +0100

    Add logging to ProductController

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using asp_tutorial.Models;
 using asp_tutorial.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,7 +13,10 @@ namespace asp_tutorial.Controllers {
     [Route("products")]
     [ApiController]
     public class ProductController : ControllerBase {
-        public ProductController(ProductService productService) {
+        private readonly ILogger _logger;
+
+        public ProductController(ILogger<ProductController> logger, ProductService productService) {
+            _logger = logger;
             this.ProductService = productService;
         }
 
@@ -20,6 +24,7 @@ namespace asp_tutorial.Controllers {
 
         [HttpGet]
         public IEnumerable<Product> GetProducts() {
+            _logger.LogInformation("Retrieving all products");
             return ProductService.GetProducts();
         }
 
@@ -27,6 +32,7 @@ namespace asp_tutorial.Controllers {
         [HttpGet]
         public Product GetProduct([FromRoute] string ProductId)
         {
+            _logger.LogInformation($"Retrieving product with id {ProductId}");
             return ProductService.GetProducts().First(product => product.Id == ProductId);
         }
 
@@ -34,6 +40,7 @@ namespace asp_tutorial.Controllers {
         [HttpGet]
         public int[] GetRatings([FromRoute] string ProductId)
         {
+            _logger.LogInformation($"Retrieving ratings for product with id {ProductId}");
             return ProductService.GetProducts().First(product => product.Id == ProductId).Ratings;
         }
 
@@ -41,6 +48,7 @@ namespace asp_tutorial.Controllers {
         [HttpPost]
         public ActionResult AddRating ([FromRoute] string ProductId, [FromForm] int rating)
         {
+            _logger.LogInformation($"Adding rating {rating} to product with id {ProductId}");
             ProductService.AddRating(ProductId, rating);
             return Ok();
         }
