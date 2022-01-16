317e7fdec9beb555864bb5352f760eb41f380693
jbrems <jonasbrems@gmail.com>
2022-01-16 14:57:17 +0100

    ASP.net ContosoCraft tutorial completion

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using asp_tutorial.Models;
+using asp_tutorial.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace asp_tutorial.Controllers {
+    [Route("products")]
+    [ApiController]
+    public class ProductController : ControllerBase {
+        public ProductController(ProductService productService) {
+            this.ProductService = productService;
+        }
+
+        public ProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts () {
+            return ProductService.GetProducts();
+        }
+
+        [Route("rate")]
+        [HttpGet]
+        public ActionResult AddRating ([FromQuery] string ProductId, [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace asp_tutorial.Models {
+    public class Product {
+        public string Id { get; set; }
+
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        public string URL { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int[] Ratings { get; set; }
+
+        public void AddRating(int rating) {
+            if (Ratings == null) Ratings = new int[] { };
+
+            List<int> ratingList = Ratings.ToList();
+            ratingList.Add(rating);
+            Ratings = ratingList.ToArray();
+        }
+
+        public override string ToString() {
+            return JsonSerializer.Serialize<Product>(this);
+        }
+    }
+}
